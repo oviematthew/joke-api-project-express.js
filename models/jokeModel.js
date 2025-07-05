1c68a3bff132ce784ad014de8f9b7b654161ed8d
MatthewEnamuotor <90224141+oviematthew@users.noreply.github.com>
2025-07-05 07:45:19 -0400

    updated api with to get id without  the id field

--- models/jokeModel.js
+++ models/jokeModel.js
@@ -2,11 +2,20 @@ const fs = require("fs");
 const path = require("path");
 const filePath = path.join(__dirname, "../data/jokes.json");
 
+// Get jokes from the JSON file
+
 function getJokes() {
-  const data = fs.readFileSync(filePath);
-  return JSON.parse(data);
+  const raw = fs.readFileSync(filePath, "utf8");
+  const jokes = JSON.parse(raw);
+
+  // Assign IDs if missing
+  return jokes.map((joke, index) => ({
+    id: index + 1, // simple incremental ID starting at 1
+    ...joke,
+  }));
 }
 
+// Save jokes to the JSON file
 function saveJokes(jokes) {
   fs.writeFileSync(filePath, JSON.stringify(jokes, null, 2));
 }
